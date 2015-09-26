ac7a9152c631933ba855be85806db9f757d9d7d8
Nicolas Bigler <nicolas.bigler@vshn.ch>
2015-09-26 04:55:54 +0200

    Add spec folders from rspec-puppet-init

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
-require 'puppetlabs_spec_helper/module_spec_helper'
+require 'rspec-puppet'
+
+fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
 RSpec.configure do |c|
-  c.hiera_config = File.expand_path(File.join(__FILE__, '../fixtures/hiera.yaml'))
+  c.module_path = File.join(fixture_path, 'modules')
+  c.manifest_dir = File.join(fixture_path, 'manifests')
+  c.environmentpath = File.join(Dir.pwd, 'spec')
 end
